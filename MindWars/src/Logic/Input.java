bdbeb2c0fb73801dfe1a831f74c3acf9abe7fcce
Benjamin <B.wie@gmx.at>
2016-02-29 17:35:34 +0100

    INPUT

---
+++ MindWars/src/Logic/Input.java
@@ -0,0 +1,35 @@
+package Logic;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import Graphics.Settings;
+
+public class Input implements KeyListener{
+	
+	private Settings s;
+	
+	public Input(Settings settings, GameCalculation menu)
+	{
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
